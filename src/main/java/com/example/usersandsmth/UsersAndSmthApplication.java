576ca675d1082dedfb1a1c21c8e0059b3851c8b5
Rafael García Pérez <p82garpr@uco.es>
2025-07-02 18:29:22 +0200

    first commit

---
+++ src/main/java/com/example/usersandsmth/UsersAndSmthApplication.java
@@ -0,0 +1,14 @@
+package com.example.usersandsmth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndSmthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersAndSmthApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/usersandsmth/UsersAndSmthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersandsmth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAndSmthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
